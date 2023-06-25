cae5f9ce974b08e7bc5f6ca5c631df6c0d1c42e2
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2023-06-25 15:48:23 +0800

    add metalink logic. [PLACEHOLDER]

---
+++ rypper-reader/src/metalink.rs
@@ -0,0 +1,48 @@
+use md5::Md5;
+use quick_xml::NsReader;
+use sha1::Sha1;
+use sha2::Sha512_256;
+use sha2::{Sha256, Sha512};
+use std::fs;
+use std::path;
+use std::sync::Arc;
+use url::Url;
+
+// We don't really care about this but for uniformity reasons
+pub struct Publisher {
+    name: String,
+    url: Url,
+}
+
+// We really care about this because lol
+pub struct FileData {
+    name: String,
+    origin: Url,
+    hash_md5: Md5,
+    hash_sha1: Sha1,
+    hash_sha256: Sha256,
+    hash_sha512: Sha512,
+    pieces: Arc<Sha1>,
+    mirrors: Location,
+}
+
+struct Location {
+    // I may use https://github.com/sifton/isocountry-rs/blob/master/src/lib.rs for this but eh not now
+    location: String,
+    priority: u8,
+    url: Url,
+}
+
+// This is the struct that we want to construct
+pub struct MetaLinkData {
+    pub generator: String,
+    pub published: String,
+    pub publisher: Publisher,
+    pub file: FileData,
+}
+
+impl MetaLinkData {}
+
+impl Location {}
+impl FileData {}
+impl Publisher {}

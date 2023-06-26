cb4e8cb847469c21aa16b46eba5f6325eda68ed8
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2023-06-26 20:45:43 +0800

    reader: update RepoConfig
    As it turns out, I don't need the `new` method
    for the implementation of `RepoConfig` struct.
    
    What I need really is just the default which is done
    by creating a `Default` type and `default` method for the
    `Default` type for `RepoConfig` which does the job
    of setting the defaults.
    
    The next goal would be to create a `write_config` method
    for default openSUSE repositories but the question is, is that
    even needed? I have a feeling that this very compatible
    with RPM-based distributions so I have to hold this out for now.
    We can always check `/etc/os-release` or
    `/etc/lsb_release` (deprecated) to write the config but I don't
    think this will happen and will cause more burden than
    what is necessary.

--- rypper-reader/src/repoconfig.rs
+++ rypper-reader/src/repoconfig.rs
@@ -1,8 +1,8 @@
 use ini::Ini;
-use std::env;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
+// NOTE: Maybe rearrange the struct fields by moving them into categories such as `RepoDescription` and `RepoOptions`.
 pub struct RepoConfig {
     pub autorefresh: bool,
     pub baseurl: String,
@@ -16,19 +16,26 @@ pub struct RepoConfig {
 
 impl Default for RepoConfig {
     fn default() -> RepoConfig {
-        let autorefresh: bool = false;
-        let enabled: bool = false;
-        let name: String = String::new();
-        let gpgcheck: bool = false;
-        let priority: u8 = 99_u8;
-        let baseurl: String = String::new();
-        let type_md = String::new();
-        let path = PathBuf::from("/");
-        RepoConfig { autorefresh, enabled, name, gpgcheck, priority, baseurl, type_md, path }
+        RepoConfig {
+            autorefresh: false,
+            enabled: false,
+            name: String::new(),
+            gpgcheck: false,
+            priority: 99_u8,
+            baseurl: String::new(),
+            type_md: String::new(),
+            path: PathBuf::from("/"),
+        }
     }
 }
 
+/*
+TODO: Write methods for default for
+default config for download-opensuse.org-oss and
+dowload-opensuse.org-non-oss
+*/
 impl RepoConfig {
+    
     pub fn load_from_file(p: &Path) -> RepoConfig {
         let conf = Ini::load_from_file(p).unwrap();
         let mut properties: &ini::Properties = &ini::Properties::new();
@@ -39,63 +46,45 @@ impl RepoConfig {
         }
 
         // Set default values. Ugly but Ugly
-        let mut autorefresh: bool = false;
-        let mut enabled: bool = false;
-        let mut name: String = String::new();
-        let mut gpgcheck: bool = false;
-        let mut priority: u8 = 99_u8;
-        let mut baseurl: String = String::new();
-        let mut type_md = String::new();
-        let mut path = PathBuf::from("/");
+        let mut default = RepoConfig::default();
         for (key, value) in properties.iter() {
             match key {
                 "autorefresh" => {
-                    autorefresh = value.parse::<u8>().unwrap() != 0;
+                    default.autorefresh = value.parse::<u8>().unwrap() != 0;
                 }
                 "enabled" => {
-                    enabled = value.parse::<u8>().unwrap() != 0;
+                    default.enabled = value.parse::<u8>().unwrap() != 0;
                 }
                 "gpgcheck" => {
-                    gpgcheck = value.parse::<u8>().unwrap() != 0;
+                    default.gpgcheck = value.parse::<u8>().unwrap() != 0;
                 }
                 "priority" => {
                     let check_priority = value.parse::<u8>().unwrap();
-                    priority = match check_priority {
+                    default.priority = match check_priority {
                         1..=99 => check_priority,
                         _ => panic!("Key {} has value {}. That's not between 1-99", key, value),
                     }
                 }
                 "baseurl" => {
-                    baseurl = value.to_string();
+                    default.baseurl = value.to_string();
                 }
                 "name" => {
-                    name = value.to_string();
+                    default.name = value.to_string();
                 }
                 "type" => {
-                    type_md = value.to_string();
+                    default.type_md = value.to_string();
                 }
                 "path" => {
-                    path = std::path::PathBuf::from(value);
+                    default.path = std::path::PathBuf::from(value);
                 }
                 _ => {
                     println!("Ignoring key {} with value {}", key, value);
                 }
             }
         }
-        RepoConfig { autorefresh, enabled, name, gpgcheck, priority, baseurl, type_md, path }
+        default
     }
-    /* TODO: Use builder pattern to fix
-    https://rust-lang.github.io/rust-clippy/master/index.html#/too_many_arguments */
-    pub fn new(
-        autorefresh: bool,
-        baseurl: String,
-        enabled: bool,
-        gpgcheck: bool,
-        name: String,
-        priority: u8,
-        type_md: String,
-        path: PathBuf,
-    ) -> Self {
-        Self { autorefresh, baseurl, enabled, gpgcheck, name, priority, type_md, path }
-    }
+    // TODO: Finish this write_config function.
+    pub fn _write_config() {}
 }
+
